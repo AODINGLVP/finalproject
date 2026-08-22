6b8cf292434843db0219b5a194a1a9f2370cc137
aodinglv-source <aodinglv@163.com>
2026-08-22 19:36:52 +0800

    Capture playable 241-node arena smoke evidence

--- testgame/testgame/tests/run_arena_smoke_test.gd
+++ testgame/testgame/tests/run_arena_smoke_test.gd
@@ -22,11 +22,14 @@ func _run() -> void:
 			active_runners += 1
 	var hud_text: String = game.get_node("UI/TopPanel/TopContent/PlayerStatus").text
 	var complex_alive: bool = game.get_node("EnemyB").health > 0 and game.get_node("EnemyC").health > 0
-	print("ARENA_SMOKE active_runners=%d player_hud_chars=%d complex_alive=%s" % [active_runners, hud_text.length(), str(complex_alive)])
+	var screenshot := root.get_texture().get_image()
+	var screenshot_path := "res://test_results/arena_playable_241.png"
+	var screenshot_error := screenshot.save_png(screenshot_path)
+	print("ARENA_SMOKE active_runners=%d player_hud_chars=%d complex_alive=%s screenshot=%s" % [active_runners, hud_text.length(), str(complex_alive), screenshot_path])
 	game.free()
 	await process_frame
 	await process_frame
-	_quit_with_result.call_deferred(0 if active_runners == 3 and hud_text.length() > 20 and complex_alive else 1)
+	_quit_with_result.call_deferred(0 if active_runners == 3 and hud_text.length() > 20 and complex_alive and screenshot_error == OK else 1)
 
 
 func _quit_with_result(code: int) -> void:
